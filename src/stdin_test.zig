09a387fb8e0e1e808f8c77732702dfaab44c5bab
Fabian Otto <fabian.otto@lambdawerk.com>
2020-05-10 21:55:51 +0200

    test read from stdin into array

---
+++ src/stdin_test.zig
@@ -0,0 +1,26 @@
+const std = @import("std");
+const stdin = std.io.getStdIn().inStream();
+const Allocator = std.mem.Allocator;
+
+const BUFSIZ = 8192;
+
+fn readLines(allocator: *Allocator) !std.ArrayList([]u8) {
+    var array = std.ArrayList([]u8).init(allocator);
+    var buffer : [BUFSIZ]u8 = undefined;
+
+    while (try stdin.readUntilDelimiterOrEof(buffer[0..], '\n')) |line| {
+        var copy = try std.mem.dupe(allocator, u8, line);
+        _ =  try array.append(copy);
+    }
+
+    return array;
+}
+
+pub fn main() !void {
+    const stdout = std.io.getStdOut().outStream();
+
+    var x = try readLines(std.heap.page_allocator);
+    for (x.items) |l| {
+        try stdout.print("{}\n", .{ l });
+    }
+}
