6fbf34309c02dc953fc660226642180a4d7fdecd
Fabian Otto <fabian.otto@lambdawerk.com>
2020-05-11 23:41:17 +0200

    cleanup

--- src/main.zig
+++ src/main.zig
@@ -3,13 +3,10 @@ const Allocator = std.mem.Allocator;
 const ArenaAllocator = std.heap.ArenaAllocator;
 const ValueTree = std.json.ValueTree;
 const Value = std.json.Value;
-
 const stdin = &std.io.getStdIn().inStream();
-
 const BUFFSIZE = 2048;
 
 var a = ArenaAllocator.init(std.heap.page_allocator);
-
 
 
 const Document = union(enum) {
@@ -66,11 +63,7 @@ const Document = union(enum) {
 
         return;
     }
-
-    
 };
-
-
 
 
 /// Returning the unprocessed argument from the command line
@@ -82,40 +75,6 @@ fn readArgs(alloc: *std.mem.Allocator) ![][]u8 {
 
     return args;
 }
-
-fn createArrayDocument(arena: *std.heap.ArenaAllocator) std.json.ValueTree {
-    return std.json.ValueTree{
-        .arena = arena.*,
-        .root = std.json.Value{ .Array = std.json.Array.init(&arena.allocator) }
-    };
-}
-
-fn createObjectDocument(arena: *std.heap.ArenaAllocator) std.json.ValueTree {
-    return std.json.ValueTree{
-        .arena = arena.*,
-        .root = std.json.Value{ .Object = std.json.ObjectMap.init(&arena.allocator) }
-    };
-}
-
-
-fn toHashMap(allocator: *std.mem.Allocator, keyValue: [] const u8) !std.json.Value {
-    var js = std.json.Value{ .Object = std.json.ObjectMap.init(allocator) };
-    var segments: std.mem.TokenIterator = std.mem.tokenize(keyValue, "=:");
-    var key: []const u8 =  undefined;
-    var value: []const u8 = undefined;
-    
-    if (segments.next()) |k| {
-        key = k;
-    }
-
-    value = segments.rest();
-    
-    _ = try js.Object.put(key, std.json.Value{ .String = value });
-
-    return js;
-}
-
-
 
 
 /// reading from stdin and returning an array of characters (string).
@@ -151,23 +110,6 @@ pub fn main() anyerror!void {
     var writer = std.json.writeStream(outstream, 10);
     try writer.emitJson(document01.array);
     try writer.emitJson(document02.object);
-    
-//     try appendToArray(&arrayDoc, "literal String");
-//     try appendToArray(&arrayDoc, "another String");
-
-//     try appendToObject(&objectDoc, "key=value");
-//     try appendToObject(&objectDoc, "key2=ovalue");
-//     try appendToObject(&objectDoc, "key3=':ovalue'");
-//     try appendToObject(&objectDoc, "key4='oval=ue'");
-    
-//     var outstream = std.io.getStdOut().outStream();
-//     var writer = std.json.writeStream(outstream, 10);
-
-//     try outstream.print("\n", .{});
-//     try writer.emitJson(arrayDoc.root);
-//     try outstream.print("\n", .{});
-//     try writer.emitJson(objectDoc.root);
-//     try outstream.print("\n", .{});
 }
 
 
@@ -256,14 +198,3 @@ test "parse json"  {
         assert(j.root == .Integer);
     }
 }
-    
-// test "read stdin and parse" {
-//     const allocator = std.teting.allocator;
-//     var arena = std.heap.ArenaAlloctor.init(allocator);
-//     defer arena.deinit();
-
-//     {
-
-    
-
-// }
