1e051dc4800bde0b0a55cac661c3e146b9143f0d
Fabian Otto <fabian.otto@lambdawerk.com>
2020-06-24 10:51:29 +0200

    commenting read from stdin test

--- src/stdin_test.zig
+++ src/stdin_test.zig
@@ -1,16 +1,23 @@
+//
+// testing reading from stdin.
+//
+
 const std = @import("std");
 const stdin = std.io.getStdIn().inStream();
 const Allocator = std.mem.Allocator;
 
 const BUFSIZ = 8192;
 
+// use zig's std lib for reading lines.
+// line length is limited to buffer length
+// append line to the ArrayList
 fn readLines(allocator: *Allocator) !std.ArrayList([]u8) {
     var array = std.ArrayList([]u8).init(allocator);
-    var buffer : [BUFSIZ]u8 = undefined;
+    var buffer: [BUFSIZ]u8 = undefined;
 
     while (try stdin.readUntilDelimiterOrEof(buffer[0..], '\n')) |line| {
         var copy = try std.mem.dupe(allocator, u8, line);
-        _ =  try array.append(copy);
+        _ = try array.append(copy);
     }
 
     return array;
@@ -21,6 +28,6 @@ pub fn main() !void {
 
     var x = try readLines(std.heap.page_allocator);
     for (x.items) |l| {
-        try stdout.print("{}\n", .{ l });
+        try stdout.print("{}\n", .{l});
     }
 }
