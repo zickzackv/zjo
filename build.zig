052ef5131b85ef7d462ae8945c10cadcce1f575f
Fabian Otto <fabian.otto@lambdawerk.com>
2020-07-19 23:52:55 +0200

    adding test runs with parameters to build

--- build.zig
+++ build.zig
@@ -1,5 +1,5 @@
 const Builder = @import("std").build.Builder;
-const debug   = @import("std").debug.warn;
+const debug = @import("std").debug.print;
 
 pub fn build(b: *Builder) void {
     // Standard target options allows the person running `zig build` to choose
@@ -13,7 +13,7 @@ pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
 
     const zjo = b.addExecutable("zjo", "src/main.zig");
-    
+
     zjo.setTarget(target);
     zjo.setBuildMode(mode);
     zjo.addPackagePath("args", "./zig-args/args.zig");
@@ -26,7 +26,23 @@ pub fn build(b: *Builder) void {
     // $zig build run -- arg1 arg2 arg3
     if (b.args) |args| run_cmd.addArgs(args);
 
-
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
+
+    const command_args = .{
+        [_][]const u8{ "-a", "zero", "one", "two", "three" },
+        [_][]const u8{ "-a", "null", "eins", "zwei", "drei", "vier" },
+        [_][]const u8{ "null:zero", "eins:oone", "zwei:2", "drei:4", "vier:9" },
+    };
+
+    const test_step = b.step("test", "rung zjo with different parameters");
+
+    inline for (command_args) |args| {
+        const command = zjo.run();
+        command.addArgs(&args);
+        command.step.dependOn(b.getInstallStep());
+        const log = b.addLog("\nRun zjo with args '{s}'\n", .{args});
+        test_step.dependOn(&command.step);
+        test_step.dependOn(&log.step);
+    }
 }
